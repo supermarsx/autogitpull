59697de20d5882bfde47894b60a7c7b0c81a0df7
Mariana <github_618a@imariana.com>
2025-08-13 21:54:09 +0100

    build(scripts): include cli command sources

--- scripts/compile-debug.sh
+++ scripts/compile-debug.sh
@@ -34,8 +34,11 @@ mkdir -p "${ROOT_DIR}/dist"
     ${ROOT_DIR}/src/lock_utils_posix.cpp \
     ${ROOT_DIR}/src/process_monitor.cpp \
     ${ROOT_DIR}/src/help_text.cpp \
+    ${ROOT_DIR}/src/cli_commands.cpp \
     ${ROOT_DIR}/src/linux_daemon.cpp \
     ${ROOT_DIR}/src/windows_service.cpp \
+    ${ROOT_DIR}/src/linux_commands.cpp \
+    ${ROOT_DIR}/src/windows_commands.cpp \
     ${PKG_LIBS} -fsanitize=address -o "${ROOT_DIR}/dist/autogitpull_debug"
 
 echo "Build complete: ${ROOT_DIR}/dist/autogitpull_debug"
